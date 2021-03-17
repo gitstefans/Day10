a9a3908155a77299ab4ef50a1d37a4520dccde53
Stefan Skočo <stefansgit@outlook.com>
2021-03-18 00:15:41 +0100

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+var myPet = {
+  species: "animal",
+  name: "Jacky",
+  legs: 4,
+  friends: ["Ax", "Benny"]
+};
+
+function myFunction(myObj) {
+  return myObj;
+}
+
+// Only change code above this line
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
